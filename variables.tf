9c81905fca0b3aaffdef8656cf5b85c28735dcc2
Richard Annand <richard+git@ohheyrj.co.uk>
2022-09-21 20:51:54 +0100

    Revert "fix: used data region to validate"
    This reverts commit 0d5b9a85da4b9e9b2ab774c9e3065a279b4e2a61.

--- variables.tf
+++ variables.tf
@@ -1,13 +1,9 @@
-data "aws_regions" "current" {
-    all_regions = true
-}
-
 variable "region" {
   description = "The AWS region to use"
   default     = "us-east-1"
   type        = string
   validation {
-    condition     = contains([aws_regions.current.name], var.region)
+    condition     = contains(["us-east-1", "us-east-2", "us-west-1", "us-west-2", "ap-east-1", "ap-south-1", "ap-northeast-3", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "ca-central-1", "cn-north-1", "cn-northwest-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-west-3", "eu-north-1", "me-south-1", "sa-east-1"], var.region)
     error_message = "Not a valid region."
   }
 }
